720309af450cb553458019b0bdec74e0b8d4891a
DeFFect76 <msevstusev@gmail.com>
2026-06-20 21:36:44 +0300

    update

--- hometasks-js.js
+++ hometasks-js.js
@@ -1,15 +1,50 @@
 // Якщо змінна більше нуля - виведіть true, менше - false
 // Перевірте це на варіантах 1, 0, -3.
 
+// const value = Number(prompt('Введіть число:'));
+
+// let x;
+
+// if (value > 0) {
+//   x = true;
+// } else {
+//   x = false;
+// }
+
+// console.log(x);
+
 // Якщо змінна ="test" - виведіть true,
 // Перевірте це на варіантах 'test'
 // ,
 // "qwerty"
 // , true
+// const testValue = prompt('Введіть слово:');
+
+// let y;
+
+// if (testValue === 'test') {
+//   y = true;
+// } else {
+//   y = false;
+// }
+
+// console.log(y);
 
 // Якщо змінна більше 10 - відніміть 5,
 //менше - додайте 5, результат виведіть в консоль
 //Перевірте це на варіантах 1, 10, 13.
+
+// const numberValue = Number(prompt('Введіть число:'));
+
+// let z;
+
+// if (numberValue > 10) {
+//   z = numberValue - 5;
+// } else {
+//   z = numberValue + 5;
+// }
+
+// console.log(z);
 
 //Зробіть сервіс який отримує число від 1 до 12
 // виведіть місяць який дорівнює числу
